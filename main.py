1bc623597004ab5d28a7729be3346e71b58630f3
Yoel <xyshell@foxmail.com>
2019-03-28 20:13:14 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+# import pandas as pd
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    dcc.Input(id='input', value='Enter something', type='text'),
+    html.Div(id='output')
+    ])
+
+@app.callback(
+    dash.dependencies.Output(component_id='output', component_property='children'),
+    [dash.dependencies.Input(component_id='input', component_property='value')])
+def update_value(input_data):
+    try:
+        return str(float(input_data)**2)
+    except:
+        return "Some error"
+if __name__ == '__main__':
+    app.run_server()
+    # pd.read_csv("SPX.csv")
+    # pd.read_csv("YieldCurve.csv")
