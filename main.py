b6af70b5458a03f24d77664002bdaa04d89e7b3e
Yoel <xyshell@foxmail.com>
2019-03-28 22:32:56 -0400

    .

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
-import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas_datareader.data as web
+import datetime
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash()
 server = app.server
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
-])
+app.layout = html.Div(
+    children=[
+        html.Div(children='''
+            symbol to graph:
+        '''),
+        dcc.Input(id='input', value='', type='text'),
+        html.Div(id='output-graph'),
+    ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+@app.callback(
+    Output(component_id='output-graph', component_property='children'),
+    [Input(component_id='input', component_property='value')])
+def update_graph(input_data):
+    start = datetime.datetime(2015, 1, 1)
+    end = datetime.datetime.now()
+    df = web.DataReader(input_data, 'yahoo', start, end)
+    return dcc.Graph(
+            id='example-graph',
+            figure={
+                'data':[
+                    {'x':df.index, 'y':df.Close, 'type':'line', 'name':input_data},
+                ],
+                'layout':{
+                    'title':input_data
+                }
+            })
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
