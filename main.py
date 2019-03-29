0263eb6a47c1f84b71f02b7a2922ff662813b26b
Yoel <xyshell@foxmail.com>
2019-03-28 21:06:16 -0400

    .

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import dash_html_components as html
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server = app.server
 app.layout = html.Div([
     html.H2('Hello World'),
     dcc.Dropdown(
